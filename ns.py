e3acda4824941c2e6ca6512959cb8a646b1dd947
Soku <soku882@gmail.com>
2025-12-03 06:29:08 -0500

    done

--- ns.py
+++ ns.py
@@ -21,15 +21,26 @@ def scan(ip):
     print("_"*45)
     print("IP\t\t\tMAC Address\n-------------------------------------------")
 
+    clients_list = []
 
     for element in answered_list:
-        print(element[1].psrc,end="\t\t")
-        print(element[1].hwsrc)
+        clients_dict = {"ip":element[1].psrc,"mac":element[1].hwsrc}
+        clients_list.append(clients_dict)
+        # print(element[1].psrc,end="\t\t")
+        # print(element[1].hwsrc)
+    return clients_list
+
+def print_result(results_list):
+
+    for element in results_list:
+        print(element["ip"], end="\t\t")
+        print(element["mac"])
+
+results_list = scan("192.168.73.2/24")
         
+print_result(results_list=results_list)
 
 
 
 
 
-
-scan("192.168.73.2/24")
